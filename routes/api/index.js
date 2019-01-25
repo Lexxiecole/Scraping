0c5fd514c569394a79bc9e9114da7d259977bf69
Lexx <Lexxiecole@gmail.com>
2019-01-25 15:57:34 -0500

    put info back in files,

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+var router = require("express").Router();
+var fetchRoutes = require("./fetch");
+var noteRoutes = require("./notes");
+var headlineRoutes = require("./headlines");
+var clearRoutes = require("./clear");
+
+
+module.exports = router;
